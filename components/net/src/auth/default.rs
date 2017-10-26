def728df61e05e623ec2a43620dcee9f2a3b6dcc
vinomca-megam <vino.v@megam.io>
2017-10-26 15:44:05 +0530

    code for erro

--- components/net/src/auth/default.rs
+++ components/net/src/auth/default.rs
@@ -170,7 +170,7 @@ pub struct AuthErr {
 
 impl fmt::Display for AuthErr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "err={}, desc={}", self.error, self.error_description)
+        write!(f, "{}", self.error)
     }
 }
 
