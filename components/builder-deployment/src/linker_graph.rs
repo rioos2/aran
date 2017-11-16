da2cbfda18e9291763870237a27b70b14aca75e6
vinomca-megam <vino.v@megam.io>
2017-11-16 19:29:16 +0530

    code for create service using graph

--- components/builder-deployment/src/linker_graph.rs
+++ components/builder-deployment/src/linker_graph.rs
@@ -1,67 +1,68 @@
-// // Copyright (c) 2017 RioCorp Inc
-// //
+// Copyright (c) 2017 RioCorp Inc
 //
-// use std::str::FromStr;
-// use std::iter::Iterator;
-// use std::collections::HashMap;
-// use protocol::{jobsrv, servicesrv};
-// use service_graph::ServiceGraph;
-// use std::collections::BTreeMap;
-//
-//
-// pub struct LinkerGraphStats {
-//     pub target: String,
-//     pub node_count: usize,
-//     pub edge_count: usize,
-// }
-//
-// pub struct LinkerGraph {
-//     graphs: HashMap<String, ServiceGraph>,
-// }
-//
-// impl LinkerGraph {
-//     pub fn new() -> Self {
-//         let mut graphs = HashMap::new();
-//
-//         for target_str in &[LoadBalancer, ExternalName] {
-//             graphs.insert(target_str, ServiceGraph::new());
-//         }
-//
-//         LinkerGraph { graphs: graphs }
-//     }
-//
-//     pub fn graph(&self, target_str: &str) -> Option<&ServiceGraph> {
-//         self.graphs.get(&target)
-//     }
-//
-//     pub fn graph_mut(&mut self, target_str: &str) -> Option<&mut ServiceGraph> {
-//         self.graphs.get_mut(&target)
-//     }
-//
-//     pub fn build<T>(&mut self, services: T) -> Map<&str, LinkerGraphStats>
-//     where
-//         T: Iterator<Item = servicesrv::Service>,
-//     {
-//         for s in services {
-//             match self.graph_mut(s.get_type()) { //LoadBalancer or ExternalName
-//                 Some(ref mut graph) => {
-//                     graph.extend(&s);
-//                 }
-//                 None => (),
-//             }
-//         }
-//
-//         let mut target_stats = BTreeMap::new();
-//         for (target, graph) in self.graphs.iter() {
-//             let stats = graph.stats();
-//             let ts = LinkerGraphStats {
-//                 target: target.clone(),
-//                 node_count: stats.node_count,
-//                 edge_count: stats.edge_count,
-//             };
-//             target_stats.insert(target.clone(), ts);
-//         }
-//
-//         target_stats
-//     }
-// }
+use std::iter::Iterator;
+use std::collections::HashMap;
+use error::{Result, Error};
+use protocol::{servicesrv, asmsrv};
+use service_graph::ServiceGraph;
+use std::collections::BTreeMap;
+use linkers_state::{LOADBALANCER, EXTERNALNAME};
+
+pub struct LinkerGraphStats {
+    pub target: String,
+    pub node_count: usize,
+    pub edge_count: usize,
+}
+
+pub struct LinkerGraph {
+    pub graphs: HashMap<String, ServiceGraph>,
+}
+
+impl LinkerGraph {
+    pub fn new() -> Self {
+        let mut graphs = HashMap::new();
+
+        for target_str in &[LOADBALANCER, EXTERNALNAME] {
+            graphs.insert(target_str.to_string(), ServiceGraph::new());
+        }
+
+        LinkerGraph { graphs: graphs }
+    }
+
+    pub fn graph(&self, target_str: &str) -> Option<&ServiceGraph> {
+        self.graphs.get(target_str)
+    }
+
+    pub fn graph_mut(&mut self, target_str: &str) -> Option<&mut ServiceGraph> {
+        self.graphs.get_mut(target_str)
+    }
+
+    pub fn build<T>(&mut self, services: T) -> BTreeMap<String, LinkerGraphStats>
+    where
+        T: Iterator<Item = Result<(Option<servicesrv::ServicesGetResponse>, Vec<asmsrv::Assembly>)>>,
+    {
+        for s in services {
+            for data in s.unwrap().0.unwrap().get_items() {
+                match self.graph_mut(&data.get_spec().get_service_type()) { //LoadBalancer or ExternalName
+                    Some(ref mut graph) => {
+                        graph.extend(&data);
+                    }
+                    None => (),
+                }
+            }
+        }
+
+        let mut target_stats = BTreeMap::new();
+        for (target, graph) in self.graphs.iter() {
+            let stats = graph.stats();
+            let ts = LinkerGraphStats {
+                target: target.clone(),
+                node_count: stats.node_count,
+                edge_count: stats.edge_count,
+            };
+            target_stats.insert(target.clone(), ts);
+        }
+
+        target_stats
+    }
+}
--- components/builder-deployment/src/linkers_state.rs
+++ components/builder-deployment/src/linkers_state.rs
@@ -1,77 +1,103 @@
-// // Copyright (c) 2017 RioCorp Inc
-// //
-// use linker_graph::LinkerGraph;
-// use linker_ds::LinkersDS;
-// use assembly_ds::AssemblyDS;
-// use protocol::servicesrv;
-// const LOADBALANCER: &'static str = "LoadBalancer";
-// const EXTERNALNAME: &'static str = "ExternalName";
+// Copyright (c) 2017 RioCorp Inc
 //
-//
-// impl ServiceState {
-//     /// Creates new `ServiceState with the assemblyfactory ids`
-//     /// The goal is to build a graph like below
-//     ///             Service id 1 => (0, node_idx1)-> (ASM1, ASM2, AS3)
-//     ///             Service id 2 => (2, node_idx2)-> (ASM4)
-//     /// This helps us cross pollinate across assembly factories.
-//     pub fn new(state_ids: Vec<af_ids>, conn: &DataStoreConn) -> ServiceState {
-//
-//         let mut graph = LinkerGraph::new();
-//
-//         let linkers = state_ids.map(|id| {
-//             let assemblys = AssemblyDS::show_by_assemblyfactory(conn, &IdGet { id })?;
-//             let services = LinkersDS::list_by_assembly_factory(conn, &IdGet { id })?;
-//             services.map(|s| (s, assemblys.items.assemblys))
-//         }); /// Vector<(service, Assemblys)>
-//
-//         let stats = graph.build(linkers.into_iter());
-//
-//         ServiceState {
-//             stats: stats,
-//             state: graph,
-//             conn: &DataStoreConn,
-//         }
-//     }
-//
-//     /// Return list of connected loadbalancers
-//     pub fn loadbalancers_connections(&self) -> Vec<Service> {
-//         self.state.get(LOADBALANCER).iter().cloned().collect()
-//     }
-//     /// Return list of our connected dns
-//     pub fn dns_connections(&self) -> Vec<Service> {
-//         self.state.get(EXTERNALNAME).iter().cloned().collect()
-//     }
-//     /// add loadbalancer links if none of the nodes exists of type LoadBalancer exists
-//     /// [LoadBalancer] = node_count=0
-//     pub fn add_loadbalancer_connection(&self, service: &Service) {
-//         if self.stats.get(LOADBALANCER).node_count <= 0 {
-//             LinkersDS::create(conn, service)
-//         }
-//
-//     }
-//     /// add dns connection if none of the nodes exists of type ExternalName
-//     /// [ExternalName] = node_count=0
-//     pub fn add_dns_connection(&self, service: &Service) {
-//         if self.stats.get(EXTERNALNAME).node_count <= 0 {
-//             LinkersDS::create(conn, service)
-//         }
-//     }
-//     /// remove loadbalancer links if none of the edges exists of type LoadBalancer exists
-//     /// [LoadBalancer] = edge_count=0
-//     pub fn remove_loadbalancer_connection(&self, service: &Service) -> bool {
-//         if self.dns_connections().is_empty() {
-//             LinkersDS::remove(conn, service)
-//         }
-//     }
-//     /// remove dns links if none of the edges exists of type ExternalName exists
-//     /// [ExternalName] = node_count=0
-//     pub fn remove_dns_connection(&self, service: &Service) -> bool {
-//         if self.dns_connections().is_empty() {
-//             LinkersDS::remove(conn, service)
-//         }
-//     }
-// }
-//
+use linker_graph::{LinkerGraph, LinkerGraphStats};
+use linker_ds::LinkersDS;
+use assembly_ds::AssemblyDS;
+use db::data_store::DataStoreConn;
+use protocol::servicesrv;
+use std::collections::BTreeMap;
+use protocol::asmsrv::IdGet;
+use service_graph::ServiceGraph;
+pub const LOADBALANCER: &'static str = "LoadBalancer";
+pub const EXTERNALNAME: &'static str = "ExternalName";
+use error::Result;
+
+pub struct LinkersState<'a> {
+    stats: BTreeMap<String, LinkerGraphStats>,
+    state: LinkerGraph,
+    conn: &'a DataStoreConn,
+}
+
+impl<'a> LinkersState<'a> {
+    /// Creates new `ServiceState with the assemblyfactory ids`
+    /// The goal is to build a graph like below
+    ///             Service id 1 => (0, node_idx1)-> (ASM1, ASM2, AS3)
+    ///             Service id 2 => (2, node_idx2)-> (ASM4)
+    /// This helps us cross pollinate across assembly factories.
+    pub fn new(state_ids: Vec<String>, conn: &'a DataStoreConn) -> LinkersState<'a> {
+
+        let mut graph = LinkerGraph::new();
+
+        let linkers = state_ids
+            .iter()
+            .map(|id| {
+                let mut id_get = IdGet::new();
+                id_get.set_id(id.to_string());
+                let assemblys = AssemblyDS::show_by_assemblyfactory(conn, &id_get);
+                let services = LinkersDS::list_by_assembly_factory(conn, &id_get);
+                services.map(|s| (s, assemblys.unwrap().unwrap().get_items()))
+            })
+            .collect::<Vec<_>>();
+
+        let stats = graph.build(linkers.into_iter());
+
+        LinkersState {
+            stats: stats,
+            state: graph,
+            conn: &*conn,
+        }
+    }
+
+    /// Return list of connected loadbalancers
+    pub fn loadbalancers_connections(&self) -> Vec<&ServiceGraph> {
+        self.state
+            .graphs
+            .get(LOADBALANCER)
+            .iter()
+            .cloned()
+            .collect::<Vec<_>>()
+    }
+    /// Return list of our connected dns
+    pub fn dns_connections(&self) -> Vec<&ServiceGraph> {
+        self.state
+            .graphs
+            .get(EXTERNALNAME)
+            .iter()
+            .cloned()
+            .collect::<Vec<_>>()
+    }
+    /// add loadbalancer links if none of the nodes exists of type LoadBalancer exists
+    /// [LoadBalancer] = node_count=0
+    pub fn add_loadbalancer_connection(&self, service: &servicesrv::Services) -> Result<Option<servicesrv::Services>> {
+        if self.stats.get(LOADBALANCER).unwrap().node_count <= 0 {
+            LinkersDS::create(self.conn, service)?;
+        }
+        Ok(None)
+    }
+    /// add dns connection if none of the nodes exists of type ExternalName
+    /// [ExternalName] = node_count=0
+    pub fn add_dns_connection(&self, service: &servicesrv::Services) -> Result<Option<servicesrv::Services>> {
+        if self.stats.get(EXTERNALNAME).unwrap().node_count <= 0 {
+            LinkersDS::create(self.conn, service)?;
+        }
+        Ok(None)
+    }
+    /*/// remove loadbalancer links if none of the edges exists of type LoadBalancer exists
+    /// [LoadBalancer] = edge_count=0
+    pub fn remove_loadbalancer_connection(&self, service: &servicesrv::Services) -> bool {
+        if self.dns_connections().is_empty() {
+            // LinkersDS::remove(self.conn, service)
+        }
+    }
+    /// remove dns links if none of the edges exists of type ExternalName exists
+    /// [ExternalName] = node_count=0
+    pub fn remove_dns_connection(&self, service: &servicesrv::Services) -> bool {
+        if self.dns_connections().is_empty() {
+            // LinkersDS::remove(self.conn, service)
+        }
+    }*/
+}
+
 // fn main() {
 //     //// Usecase 1: New assemblyfactory trying to add dns service
 //     let dns = servicesrv::Services::new(); //create a "From" to Service
