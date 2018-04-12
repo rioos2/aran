655c3a541c8205cbff8cf3c1b245db048a96fd57
suganyakaliyamoorthy <suganyakaliyamoorthy@megam.io>
2018-04-12 13:50:13 +0530

    add datacenter update

--- components/rioos/src/command/cluster/edit.rs
+++ components/rioos/src/command/cluster/edit.rs
@@ -3,22 +3,23 @@
 use common::ui::UI;
 use error::Result;
 use api_client::Client;
-use protocol::api::network;
+use protocol::api::{network, storage};
 use serde_json;
 
-
-
-pub fn edit_datacenter(ui: &mut UI, _rio_client: Client, _id: &str, _token: &str, _email: &str) -> Result<()> {
-    let data = ui.edit(&["data"])?;
-    println!("{:?}", data);
+pub fn edit_datacenter(ui: &mut UI, rio_client: Client, id: &str, token: &str, email: &str) -> Result<()> {
+    let result: storage::DataCenter = rio_client.datacenter_get_by_id(&token, &email, &id)?;
+    let data = ui.edit(&result)?;
+    let datacenter: storage::DataCenter = serde_json::from_str(&data.to_string())?;
+    rio_client.datacenter_update(&token, &email, datacenter)?;
+    ui.end("Your datacenter updated successfully!.")?;
     Ok(())
 }
 
 pub fn edit_network(ui: &mut UI, rio_client: Client, id: &str, token: &str, email: &str) -> Result<()> {
     let result: network::Network = rio_client.network_get_by_id(&token, &email, &id)?;
     let data = ui.edit(&result)?;
-    let p: network::Network = serde_json::from_str(&data.to_string()).unwrap();
-    rio_client.network_update(&token, &email, p)?;
-    ui.end("Your network successfully updated!")?;
+    let network: network::Network = serde_json::from_str(&data.to_string())?;
+    rio_client.network_update(&token, &email, network)?;
+    ui.end("Your network updated successfully!.")?;
     Ok(())
 }
--- components/rioos/src/command/datacenter/get.rs
+++ components/rioos/src/command/datacenter/get.rs
@@ -2,6 +2,9 @@ pub use error::{Error, Result};
 
 use common::ui::UI;
 use api_client::Client;
+use protocol::api::storage::DataCenter;
+use protocol::api::base::MetaFields;
+
 
 use super::super::common::pretty_table;
 
@@ -10,9 +13,18 @@ pub fn start(ui: &mut UI, rio_client: Client, token: String, email: String, id: 
         &format!("Constructing a {} datacenter for you...", id),
     )?;
     ui.br()?;
-    let result = rio_client.datacenter_get_by_id(&token, &email, &id)?;
+    let dc: DataCenter = rio_client.datacenter_get_by_id(&token, &email, &id)?;
     let title = row!["Id", "Name", "Enabled", "Status", "Hrs Ago"];
-    pretty_table(result.to_owned(), title);
+    let data = vec![
+        vec![
+            dc.get_id(),
+            dc.object_meta().name,
+            dc.get_enabled().to_string(),
+            dc.get_status().get_phase(),
+            dc.get_created_at(),
+        ],
+    ];
+    pretty_table(data.to_owned(), title);
 
     ui.para(
         "For more information on digitalclouds datacenter: \
