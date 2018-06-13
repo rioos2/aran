6c20f31c90d0960f24f98b1935236bd41cccad2a
rajthilak <rajthilak@megam.io>
2018-06-13 10:12:26 +0000

    enable openio bucket create/list/upload/download api's

--- components/builder-apimachinery/src/api/objectstorage.rs
+++ components/builder-apimachinery/src/api/objectstorage.rs
@@ -1,6 +1,82 @@
 // Copyright 2018 The Rio Advancement Inc
-use api::base::{TypeMeta, ObjectMeta, Status, MetaFields};
+use api::base::{TypeMeta, ObjectMeta, Status, MetaFields, WhoAmITypeMeta};
 use std::collections::BTreeMap;
+
+#[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
+pub struct BucketAccessorData {
+    rioos_sh_remote_url: String,
+    content_type: String,
+    date: String,
+    authorization: String,
+}
+
+impl BucketAccessorData {
+    pub fn new() -> BucketAccessorData {
+        ::std::default::Default::default()
+    }  
+    pub fn set_url(&mut self, v: ::std::string::String) {
+        self.rioos_sh_remote_url = v;
+    }
+    pub fn set_content_type(&mut self, v: ::std::string::String) {
+        self.content_type = v;
+    }
+    pub fn set_date(&mut self, v: ::std::string::String) {
+        self.date = v;
+    }
+    pub fn set_authorization(&mut self, v: ::std::string::String) {
+        self.authorization = v;
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
+pub struct BucketAccessor {
+    type_meta: TypeMeta, //Standard type metadata: kind: EndPoints
+    object_meta: ObjectMeta, //Standard object metadata
+    data: BucketAccessorData
+}
+
+impl BucketAccessor {
+    pub fn new() -> BucketAccessor {
+        ::std::default::Default::default()
+    }    
+    pub fn with(t: TypeMeta, o: ObjectMeta) -> BucketAccessor {
+        BucketAccessor {
+            type_meta: t,
+            object_meta: o,
+            ..Default::default()
+        }
+    }
+    ///  Implementing structure will send their BucketAccessorData
+    pub fn accessor_data(&self) -> BucketAccessorData {
+        BucketAccessorData::new()
+    }    
+    
+    pub fn set_accessor_data(&mut self, current: BucketAccessorData) {
+        self.data = current
+    }
+}
+
+impl MetaFields for BucketAccessor {
+    /// Returns the latest self with built ObjectMeta and Type_meta
+    /// Wipes out the old meta.
+    /// Should be handled externally by doing Meta::with(by mutating the old ObjectMeta)
+    fn set_meta(&mut self, t: TypeMeta, v: ObjectMeta) {
+        self.type_meta = t;
+        self.object_meta = v;
+    }
+
+    fn object_meta(&self) -> ObjectMeta {
+        self.object_meta.clone()
+    }
+
+    fn type_meta(&self) -> TypeMeta {
+        self.type_meta.clone()
+    }
+}
+
+impl WhoAmITypeMeta for BucketAccessor {
+    const MY_KIND: &'static str = "POST:bucketsfilesupload";
+}
 
 #[derive(Debug, PartialEq, Clone, Default, Serialize, Deserialize)]
 pub struct Bucket {
@@ -8,10 +84,7 @@ pub struct Bucket {
     id: String, // Id an unique identifier in systems of record. Generated during creation of the AssemblyFactory
     object_meta: ObjectMeta, //Standard object metadata
     #[serde(default)]
-    type_meta: TypeMeta, //standard type metadata: kind: Bucket
-    parameters: BTreeMap<String, String>,
-    status: Status, //Most recently observed status of the service. Populated by the system. Read-only.  Initially during submission, the status is "pending"
-    created_at: String,
+    type_meta: TypeMeta //standard type metadata: kind: Bucket   
 }
 impl Bucket {
     pub fn new() -> Bucket {
@@ -31,32 +104,13 @@ impl Bucket {
     }
     pub fn get_id(&self) -> ::std::string::String {
         self.id.clone()
-    }
-
-    pub fn set_status(&mut self, v: Status) {
-        self.status = v;
-    }
-
-    pub fn get_status(&self) -> &Status {
-        &self.status
-    }
-
-    pub fn set_paramaters(&mut self, v: BTreeMap<String, String>) {
-        self.parameters = v;
-    }
-
-    pub fn get_parameters(&self) -> &BTreeMap<String, String> {
-        &self.parameters
-    }
-
-    pub fn set_created_at(&mut self, v: ::std::string::String) {
-        self.created_at = v;
-    }
-
-    pub fn get_created_at(&self) -> ::std::string::String {
-        self.created_at.clone()
-    }
+    }   
 }
+
+impl WhoAmITypeMeta for Bucket {
+    const MY_KIND: &'static str = "POST:buckets";
+}
+
 impl MetaFields for Bucket {
     /// Returns the latest self with built ObjectMeta and Type_meta
     /// Wipes out the old meta.
