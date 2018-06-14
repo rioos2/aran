0eedba7ca36c6898a4395f2cc708043141500c51
Kishorekumar Neelamegam <kishore.neelamegam@rio.company>
2018-06-14 23:35:57 +0530

    The sqlformatter messed up by padding @ in .sql files.

--- components/builder-db/api/migrations/2018-06-08-153101_create_endpoints/up.sql
+++ components/builder-db/api/migrations/2018-06-08-153101_create_endpoints/up.sql
@@ -85,7 +85,7 @@ BEGIN
    FROM
       endpoints 
    WHERE
-      object_meta @ > json_build_object('owner_references', json_build_array(json_build_object('uid', pid)))::jsonb;
+      object_meta @> json_build_object('owner_references', json_build_array(json_build_object('uid', pid)))::jsonb;
 RETURN;
 END
 $$ LANGUAGE PLPGSQL STABLE;
