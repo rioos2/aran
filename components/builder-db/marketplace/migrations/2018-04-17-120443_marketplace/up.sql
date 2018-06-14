73f49e5ec5eb6ca84e65cab70910f9158513074c
rathish <rathishvbr@outlook.com>
2018-06-14 12:10:53 +0530

    plans field updated on DB

--- components/builder-db/marketplace/migrations/2018-04-17-120443_marketplace/up.sql
+++ components/builder-db/marketplace/migrations/2018-04-17-120443_marketplace/up.sql
@@ -3,14 +3,14 @@
 CREATE SEQUENCE IF NOT EXISTS market_id_seq;
 
 
-CREATE TABLE IF NOT EXISTS marketplaces (id bigint PRIMARY KEY DEFAULT next_id_v1('market_id_seq'), type_meta JSONB, object_meta JSONB, category text, VERSION text, icon text, description text, status JSONB, created_at timestamptz DEFAULT now());
+CREATE TABLE IF NOT EXISTS marketplaces (id bigint PRIMARY KEY DEFAULT next_id_v1('market_id_seq'), type_meta JSONB, object_meta JSONB, plans JSONB, category text, VERSION text, icon text, description text, status JSONB, created_at timestamptz DEFAULT now());
 
 
-CREATE OR REPLACE FUNCTION insert_marketplace_v1 (type_meta JSONB, object_meta JSONB, category text, VERSION text, icon text, description text, status JSONB) RETURNS
+CREATE OR REPLACE FUNCTION insert_marketplace_v1 (type_meta JSONB, object_meta JSONB, plans JSONB,  category text, VERSION text, icon text, description text, status JSONB) RETURNS
 SETOF marketplaces AS $$
                   BEGIN
-                    RETURN QUERY INSERT INTO marketplaces(type_meta, object_meta, category, version, icon, description, status)
-                      VALUES (type_meta, object_meta, category, version, icon, description, status)
+                    RETURN QUERY INSERT INTO marketplaces(type_meta, object_meta, plans, category, version, icon, description, status)
+                      VALUES (type_meta, object_meta, plans, category, version, icon, description, status)
                       RETURNING *;
                     RETURN;
                   END
