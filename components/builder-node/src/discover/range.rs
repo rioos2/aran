8aa890211c9644ce5e7456d7ab172095bba984c2
vino <vino.v@megam.io>
2018-08-28 14:45:57 +0000

    fix for issue #276,#275

--- components/builder-node/src/discover/range.rs
+++ components/builder-node/src/discover/range.rs
@@ -1,3 +1,4 @@
+use super::IpType;
 use ipnet::{IpAddrRange, Ipv4AddrRange, Ipv6AddrRange};
 use std::net::IpAddr;
 
@@ -17,34 +18,23 @@ impl Range {
     }
     pub fn get_ip_list(&self) -> Vec<String> {
         let ips = match IpType::find(&self.ip_type) {
-            IpType::IPV4 => IpAddrRange::from(Ipv4AddrRange::new(
-                self.from.parse().unwrap(),
-                self.to.parse().unwrap(),
-            )).collect::<Vec<IpAddr>>(),
-            IpType::IPV6 => IpAddrRange::from(Ipv6AddrRange::new(
-                self.from.parse().unwrap(),
-                self.to.parse().unwrap(),
-            )).collect::<Vec<IpAddr>>(),
+            IpType::IPV4 => {
+                IpAddrRange::from(Ipv4AddrRange::new(
+                    self.from.parse().expect("Unable to parsesocket address"),
+                    self.to.parse().expect("Unable to parsesocket address"),
+                )).collect::<Vec<IpAddr>>()
+            }
+            IpType::IPV6 => {
+                IpAddrRange::from(Ipv6AddrRange::new(
+                    self.from.parse().expect("Unable to parsesocket address"),
+                    self.to.parse().expect("Unable to parsesocket address"),
+                )).collect::<Vec<IpAddr>>()
+            }
         };
         let mut x = vec![];
         for d in ips {
             x.push(format!("{}", d));
         }
         x
-    }
-}
-
-enum IpType {
-    IPV4,
-    IPV6,
-}
-
-impl IpType {
-    pub fn find(range: &str) -> IpType {
-        match &range[..] {
-            "IPV4" => IpType::IPV4,
-            "IPV6" => IpType::IPV6,
-            _ => IpType::IPV4,
-        }
     }
 }
--- components/builder-node/src/discover/search.rs
+++ components/builder-node/src/discover/search.rs
@@ -12,19 +12,17 @@ impl Nodes {
         Nodes { item: item }
     }
     pub fn discovered(&self) -> Result<Vec<String>> {
-        let list_ip =
-            match DiscoverModes::convert(self.item.get_cidrs(), self.item.get_range_address_from())
-            {
-                DiscoverModes::InMasterSubnet  => vec!["".to_string()],
-                DiscoverModes::InProvidedCidrs => {
-                    cidrs::Cidrs::new(self.item.get_cidrs()).get_ip_list()
-                }
-                DiscoverModes::InIpAddressRange => range::Range::new(
+        let list_ip = match DiscoverModes::convert(self.item.get_cidrs(), self.item.get_range_address_from()) {
+            DiscoverModes::InMasterSubnet => vec!["".to_string()],
+            DiscoverModes::InProvidedCidrs => cidrs::Cidrs::new(self.item.get_cidrs(), self.item.get_ip_type()).get_ip_list(),
+            DiscoverModes::InIpAddressRange => {
+                range::Range::new(
                     self.item.get_range_address_from(),
                     self.item.get_range_address_to(),
                     self.item.get_ip_type(),
-                ).get_ip_list(),
-            };
+                ).get_ip_list()
+            }
+        };
         let mut ips = vec![];
         let mut ping = Ping::new();
         for d in list_ip {
